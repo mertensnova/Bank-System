132092914aa2ae20b800e2c2738ed71571669d25
amr8644 <amr8644@gmail.com>
2023-02-11 18:10:55 +0300

    Fixed read file bug

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include "Person.h"
-#include "Method.h"
-
+#include "Methods.h"
 using namespace std;
-
-
-
 
 
 int main(int argc, char const *argv[])
 {
-   menu();
+	menu();
 
     return 0;
 }
--- methods.cpp
+++ methods.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include "Person.h"
+#include "Methods.h"
 
 using namespace std;
 
-void account_create();
-void account_view_all();
 void menu()
 {
      cout << "\t\t\t********************** Banking System****************************" << endl;
      cout << "\t\t\t********************** Main menu ****************************" << endl;
      int choice;
 
-    cout << "\t1- Create an account" << endl;
-    cout << "\t2- Login to an account" << endl;
-    cout << "\t3- Show all accounts" << endl;
-    cout << "\t4- Delete an account" << endl;
+    cout << "\t1 - Create an account" << endl;
+    cout << "\t2 - Login to an account" << endl;
+    cout << "\t3 - Show all accounts" << endl;
+    cout << "\t4 - Delete an account" << endl;
     cout << "\tChoose a number: ";
     cin >> choice;
    
@@ -23,7 +23,7 @@ void menu()
      case 1:
         account_create();
         break;
-    case 2:
+    case 3:
         account_view_all();
         break;
      default:
@@ -33,9 +33,11 @@ void menu()
 
 void account_create()
 {
-    FILE *fp;
-    fp = fopen("Accounts.dat", "ab+");
+    ofstream fp_obj;
+    fp_obj.open("Accounts.dat",ios::app);
+    
     Person account;
+
     time_t now = time(0);
     char* dt = ctime(&now);
     tm *gmtm = gmtime(&now);
@@ -52,20 +54,26 @@ void account_create()
 
     cout << "Created at: " << dt;
     account.created_at  = dt;    
-    fwrite(&account, sizeof(account), 1, fp);
+
+    fp_obj.write((char*)&account,sizeof(account));
+
+    fp_obj.close();
 }
+
 void account_view_all()
 {
     Person account;
-    FILE *fp;
-    fp = fopen("Accounts.dat", "rb");
-    while (fread(&account, sizeof(account), 1, fp))
-    {
-        cout << "\t\t----------------------------------------- \n";
-        cout << "\t\tAccount's ID: " << account.id << endl;
-        cout << "\t\tAccount's name: " << account.name << endl;
-        cout << "\t\tAccount's balance: $" << account.balance << endl;
-        cout << "\t\tMember since: " << account.created_at << endl;
-  }
+    ifstream fp_obj;
+    fp_obj.open("Accounts.dat",ios::app);
+    fp_obj.read((char*)&account, sizeof(account));
+    fp_obj.seekg(0);
+    cout << "\t\t----------------------------------------- \n";
+    cout << "\t\tAccount's ID: " << account.id << endl;
+    cout << "\t\tAccount's name: " << account.name << endl;
+    cout << "\t\tAccount's balance: $" << account.balance << endl;
+    cout << "\t\tMember since: " << account.created_at << endl;
+    fp_obj.close();  
     
 }
+
+
