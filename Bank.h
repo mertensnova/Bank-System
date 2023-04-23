558acf7b4dd95f1991c6d3886d38ab9edea5d445
amr8644 <amr8644@gmail.com>
2023-04-24 01:02:31 +0300

    Fixed SQLite issues

--- Bank.h
+++ Bank.h
@@ -50,11 +50,13 @@ void Bank::bank_menu(sqlite3 &db, std::string acc_no, double balance) {
 void Bank::bank_login(sqlite3 &db) {
 
   std::string acc_no, pin;
+
+  double balance;
   char *err_msg;
-  std::cout << "Enter yout account number: ";
+  std::cout << "[*] Enter yout account number: ";
 
   std::getline(std::cin >> std::ws, acc_no);
-  std::cout << "Enter your pin number: ";
+  std::cout << "[*] Enter your pin number: ";
   std::cin >> pin;
 
   sqlite3_stmt *stmt;
@@ -64,30 +66,34 @@ void Bank::bank_login(sqlite3 &db) {
   sqlite3_prepare_v2(&db, sql.c_str(), -1, &stmt, NULL);
 
   sqlite3_bind_text(stmt, 1, acc_no.c_str(), acc_no.length(), NULL);
-  sqlite3_bind_text(stmt, 1, pin.c_str(), pin.length(), NULL);
-  while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-    printf("--------------------------------------------------\n");
-    printf("Name: %s\n", sqlite3_column_text(stmt, 1));
-    printf("Pin: %s\n",sqlite3_column_text(stmt, 2));
-    printf("Balance: %lf\n", sqlite3_column_double(stmt, 4));
-    printf("Created at: %s\n", sqlite3_column_text(stmt, 3));
-    printf("--------------------------------------------------\n");
+  sqlite3_bind_text(stmt, 2, pin.c_str(), pin.length(), NULL);
+  if ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+    balance = sqlite3_column_double(stmt, 4);
+    bank_menu(db, acc_no, balance);
   }
 }
 
 void Bank::bank_withdraw(sqlite3 &db, std::string acc_no, double balance) {
 
+  double amount;
   sqlite3_stmt *stmt;
   int rc;
+  std::cout << "[*] How much do you want to withdraw?";
+  std::cin >> amount;
+  if (amount > balance) {
+    std::cout << "[!] Not enought balance"
+              << "\n";
+    return;
+  }
   std::string sql = "UPDATE Accounts set balance = ? where name = ?;";
 
   sqlite3_prepare_v2(&db, sql.c_str(), -1, &stmt, NULL);
 
-  sqlite3_bind_text(stmt, 1, acc_no.c_str(), -1, 0);
   if (stmt != NULL) {
     sqlite3_bind_text(stmt, 1, acc_no.c_str(), acc_no.length(),
                       SQLITE_TRANSIENT);
-    sqlite3_bind_double(stmt, 3, 1200);
+
+    sqlite3_bind_double(stmt, 2, balance - amount);
     sqlite3_step(stmt);
     sqlite3_finalize(stmt);
   } else
